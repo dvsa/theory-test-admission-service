9c2cd217669ad5ec0f27e8df67860acfe71c6157
Haotian Sun <h.sun@kainos.com>
2018-03-14 11:50:57 +0000

    Reconstructed the test folder to separate the integration tests from unit tests.

--- theory-test-start-admission/test/test.js
+++
@@ -1,9 +0,0 @@
-const logger = require('logger');
-
-describe('Example Unit Test', () => {
-	describe('greeting', () => {
-		it('is "Hello World!"', () => {
-			logger.info('Hello World from Logger');
-		});
-	});
-});
