5b7fee21f123e24d92a1732705a30edb9b4c22d7
Haotian Sun <h.sun@kainos.com>
2018-03-14 12:22:00 +0000

    Deleted the unused the unit test for now as the logger module is not working properly for pipeline.

--- theory-test-start-admission/test/unit/test.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../../../logger/index');
-
-describe('Example Unit Test', () => {
-	describe('greeting', () => {
-		it('is "Hello World!"', () => {
-			logger.info('Hello World from Logger');
-		});
-	});
-});
