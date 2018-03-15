9f9ebd0b6c27005f4187fc34dfd70d314763dd57
Juan Carlos Ortega Contreras <juan.ortega-contreras@capgemini.com>
2018-03-15 09:29:50 +0000

    add skeleton code including correct input and output of each lambda  so they can be used to test the state machine Complete-Candidate-Admission

--- theory-test-detect-impersonator/lambda.js
+++ theory-test-detect-impersonator/lambda.js
@@ -20,10 +20,15 @@ exports.handler = (event, context, callback) => {
 	// log inbound event
 	logger.debug('Received event: ', JSON.stringify(event));
 
-	// invoke business logic
-	const result = main.greeting();
-
-	// return success
-	exit(callback, null, result);
-
+  let response;
+  if (event.found_matches) {
+    response = {
+      suspect_detected: true
+    };
+  } else {
+    response = {
+      suspect_detected: false
+    };
+  }
+  callback(null, response);
 };
