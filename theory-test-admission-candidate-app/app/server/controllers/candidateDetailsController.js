49a92dbc3f76a5e0d09e710b53a2ace646b1f4ef
nathanaelh <nathanaelh@kainos.com>
2018-03-15 15:05:20 +0000

    Updated env variable

--- theory-test-admission-candidate-app/app/server/controllers/candidateDetailsController.js
+++ theory-test-admission-candidate-app/app/server/controllers/candidateDetailsController.js
@@ -1,6 +1,7 @@
 import uuid from 'uuid/v4';
 import CandidateCheckBookingService from '../services/candidateCheckBookingService';
 import sessionStorage from '../middleware/manuallySetSessionData';
+const logger = require('logger');
 
 
 export default class CandidateDetailsController {
@@ -11,11 +12,15 @@ export default class CandidateDetailsController {
 		const admissionsId = uuid();
 		const candidateDLN = req.body.DLN.toString().toUpperCase();
 		CandidateCheckBookingService.checkCandidateBooking(admissionsId, candidateDLN, (hasBooking) => {
+			logger.info('GotBooking: ', hasBooking);
 			sessionStorage(req, res, 'bookingResult', { hasBooking, admissionsId });
+			logger.info('Stored in session storage');
 			if (hasBooking) {
+				logger.info('has booking');
 				sessionStorage(req, res, 'videoTermsAgreed', false);
 				res.redirect('/candidate/prepare-video');
 			} else {
+				logger.info('no booking');
 				res.redirect('/candidate/report-reception');
 			}
 		});
