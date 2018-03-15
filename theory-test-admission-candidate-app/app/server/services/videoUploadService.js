f50c1b6d3a23691ebca355f0778e47a52f4d1953
Haotian Sun <h.sun@kainos.com>
2018-03-15 09:54:18 +0000

    Added the endpoint to generate the video upload url from S3 bucket.

--- theory-test-admission-candidate-app/app/server/services/videoUploadService.js
+++ theory-test-admission-candidate-app/app/server/services/videoUploadService.js
@@ -3,7 +3,7 @@ const videoUtilities = require('candidate-video-utilities');
 export default class VideoUploadService {
 
 	/**
-	 *
+	 * This method calls the video utilities module to retrieve the upload url
 	 * @param admissionId
 	 * @param mimeType
 	 * @returns {Promise<string>}
