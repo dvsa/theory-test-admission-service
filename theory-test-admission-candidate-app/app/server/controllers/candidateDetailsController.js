67aacdd09328cd072e910281cfb99527e5d10b28
nathanaelh <nathanaelh@kainos.com>
2018-03-15 11:26:46 +0000

    Updated

--- theory-test-admission-candidate-app/app/server/controllers/candidateDetailsController.js
+++ theory-test-admission-candidate-app/app/server/controllers/candidateDetailsController.js
@@ -10,15 +10,14 @@ export default class CandidateDetailsController {
 	checkBooking(req, res) {
 		const admissionsId = uuid();
 		const candidateDLN = req.body.DLN.toString().toUpperCase();
-		this.candidateBookingService.admissionsId = admissionsId;
-		this.candidateBookingService.candidateDLN = candidateDLN;
-		const hasBooking = this.candidateBookingService.checkCandidateBooking();
-		sessionStorage(req, res, 'bookingResult', { hasBooking, admissionsId });
-		if (hasBooking) {
-			sessionStorage(req, res, 'videoTermsAgreed', false);
-			res.redirect('/candidate/prepare-video');
-		} else {
-			res.redirect('/candidate/report-reception');
-		}
+		CandidateCheckBookingService.checkCandidateBooking(admissionsId, candidateDLN, (hasBooking) => {
+			sessionStorage(req, res, 'bookingResult', { hasBooking, admissionsId });
+			if (hasBooking) {
+				sessionStorage(req, res, 'videoTermsAgreed', false);
+				res.redirect('/candidate/prepare-video');
+			} else {
+				res.redirect('/candidate/report-reception');
+			}
+		});
 	}
 }
--- theory-test-admission-candidate-app/app/server/services/candidateCheckBookingService.js
+++ theory-test-admission-candidate-app/app/server/services/candidateCheckBookingService.js
@@ -1,24 +1,55 @@
 const AWS = require('aws-sdk');
 const logger = require('logger');
+const moment = require('moment');
+
+
 
 export default class CandidateCheckBookingService {
 
-	checkCandidateBooking() {
+	static checkCandidateBooking(admissionId, candidateDLN, callback) {
 		const bookingStepFunction = new AWS.StepFunctions();
+		console.log('getting after declaration ');
+		const currentDate = moment(Number.parseInt(new Date().getTime(), 10)).toISOString(true);
+
+		const dln = `"DrivingLicenceNumber": "${candidateDLN}"`;
+		const aId = `"AdmissionId": "${admissionId}"`;
+		const date = `"Date": "${currentDate}"`;
+		const completeInput = `{ "Request": {${dln}, ${aId}, ${date}} }`;
+
 		const params = {
-			stateMachineArn: process.env.SFN_START_CANDIDATE_ADMISSION_ARN,
-			input: { Request: { DrivingLicenceNumber: this.candidateDLN, AdmissionId: this.admissionID, Date: new Date() } }
+			stateMachineArn: ,
+			input: completeInput
 		};
 		bookingStepFunction.startExecution(params, (err, data) => {
 			if (err) {
 				logger.error(err);
 				console.log('Error is: ', err);
-				return false; // an error occurred
+				return callback(false); // an error occurred
 			}
 			logger.info(data);
-			console.log('Data is: ', data);
-			return data.HasBooking; // successful response
+			console.log('Data is: ', data.executionArn);
+			const descParams = {
+				executionArn: data.executionArn,
+			};
+			bookingStepFunction.describeExecution(descParams, (error, response) => {
+				while (response.status === 'RUNNING') {
+					console.log('Running loop');
+					bookingStepFunction.describeExecution(descParams, (loopError, loopResponse) => {
+						console.log('doing describe');
+						if (loopResponse.status === 'SUCCEEDED') {
+							console.log('got response: ', loopResponse);
+							return callback(loopResponse.output.HasBooking);
+						}
+						return false;
+					});
+				}
+				console.log('Describer exec: ', response);
+				return callback(false); // successful response
+
+			});
+			return callback(false);
 		});
+
 
 	}
 }
