513a153436e3cb3a780a75ed66a6ac3729b00016
nathanaelh <nathanaelh@kainos.com>
2018-03-19 23:47:30 +0000

    Removed 'Get-candidate-entitlements' from build script

--- build.sh
+++ build.sh
@@ -21,7 +21,6 @@ declare -a PROJECTS=(
     "theory-test-complete-admission"
     "theory-test-detect-impersonator"
     "theory-test-fetch-candidate-image"
-    "theory-test-get-candidate-entitlements"
     "theory-test-recognise-candidate"
     "theory-test-start-admission"
     "theory-test-admission-candidate-app"
