3cd291ee955d299a47c261cbadb0a4a77f5811a4
Juan Carlos Ortega Contreras <juan.ortega-contreras@capgemini.com>
2018-03-21 09:09:47 +0000

    working version of fetch images form dvla lambda using hard coded variables

--- theory-test-fetch-candidate-image/lambda.js
+++ theory-test-fetch-candidate-image/lambda.js
@@ -1,4 +1,13 @@
 const logger = require('logger');
+const AWS = require('aws-sdk');
+
+const s3 = new AWS.S3();
+
+// following values to be replaced by env vars . ej process.env.ADMISSION_BUCKET;
+const SOURCE_BUCKET = 'dvsa.ftts.adm.juan-admissions';
+const SOURCE_DIRECTORY = 'one-DVLA-images';
+const DESTINATION_BUCKET = 'dvsa.ftts.adm.juan-admissions';
+const DESTINATION_DIRECTORY = 'one-candidate-photo';
 
 /**
  * Inform AWS that our Lambda's execution is complete.
@@ -15,15 +24,27 @@ function exit(callback, error, response) {
 }
 
 exports.handler = (event, context, callback) => {
-
 	// log inbound event
 	logger.debug('Received event: ', JSON.stringify(event));
 
+	const params = {
+		CopySource: `/${SOURCE_BUCKET}/${SOURCE_DIRECTORY}/${event.Request.DrivingLicenceNumber}.jpg`,
+		Bucket: DESTINATION_BUCKET,
+		Key: `/${DESTINATION_DIRECTORY}/${event.Request.AdmissionId}.jpg`
+	};
 
-	// return success
-	exit(callback, null, {
-		bucketName: 'bucketName',
-		key: 'path_to_image'
+	logger.debug('params : ', JSON.stringify(params));
+
+	s3.copyObject(params, (err, data) => {
+		let result = null;
+		if (err) console.log(err, err.stack); // an error occurred
+		else {
+			console.log(JSON.stringify(data));
+			result = {
+				Bucket: params.Bucket,
+				Key: params.Key
+			};
+		}
+		exit(callback, null, result);
 	});
-
 };
