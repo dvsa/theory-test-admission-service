bac28a51932cc7225ca2b1a6a5bdba59d895a209
Oliver Wilson <oliverw@kainos.com>
2018-03-21 17:06:32 +0000

    Tt 766 transcode candidate video (#50)
    Introduce new Lambda to transcode candidate videos.

---
+++ theory-test-transcode-candidate-video/src/transcoder.js
@@ -0,0 +1,104 @@
+const AWS = require('aws-sdk');
+
+/**
+ * 'Generic 1080p'
+ *
+ * @see https://docs.aws.amazon.com/elastictranscoder/latest/developerguide/system-presets.html
+ * @type {string}
+ */
+const DEFAULT_PRESET_ID = '1351620000001-000001';
+
+const PIPELINE_ID = process.env.ELASTIC_TRANSCODER_PIPELINE_ID;
+const PRESET_ID = process.env.ELASTIC_TRANSCODER_PRESET_ID || DEFAULT_PRESET_ID;
+
+const POLLING_INTERVAL = 250; // msec
+
+class Transcoder {
+
+	/**
+	 * @param transcoder AWS Service object; optional
+	 */
+	constructor(transcoder) {
+		this.transcoder = transcoder || new AWS.ElasticTranscoder();
+	}
+
+	/**
+	 * @
+	 * @returns {Promise<void>}
+	 */
+	transcode(input, output) {
+		return this.enqueueJob(input, output)
+			.then((id) => {
+				return this.awaitJobCompletion(id);
+			});
+	}
+
+	/**
+	 * Promises to return the job identifier of a newly enqueued Elastic Transcoder job.
+	 *
+	 * N.B. you don't get to specify the S3 Bucket which contains the files,
+	 * that's already defined in the Elastic Transcoder Pipeline.
+	 *
+	 * @private
+	 * @params {string} input filename
+	 * @params {string} output filename
+	 * @returns {Promise<string>}
+	 */
+	enqueueJob(input, output) {
+		const params = {
+			PipelineId: PIPELINE_ID,
+			Input: {
+				Key: input
+			},
+			Output: {
+				Key: output,
+				PresetId: PRESET_ID
+			}
+		};
+		return this.transcoder.createJob(params)
+			.promise()
+			.then((response) => {
+				return response.Job.Id;
+			});
+	}
+
+	/**
+	 * Promises to tell you when the specified Elastic Transcoder job has completed.
+	 *
+	 * @private
+	 * @param {string} id
+	 * @returns {Promise<void>}
+	 */
+	awaitJobCompletion(id) {
+		return this.transcoder.readJob({ Id: id })
+			.promise()
+			.then((response) => {
+				switch (response.Job.Status) {
+				case 'Complete':
+					return undefined; // success
+				case 'Submitted':
+				case 'Progressing':
+					return Transcoder.sleep()
+						.then(() => {
+							return this.awaitJobCompletion.apply(this, [id]);
+						});
+				case 'Canceled': // yes, that is the correct spelling
+				case 'Error':
+				default:
+					throw new Error(`Elastic Transcoder job has status '${response.Job.Status}'!`);
+				}
+			});
+	}
+
+	/**
+	 * @returns {Promise<void>}
+	 */
+	static sleep() {
+		return new Promise((resolve) => {
+			setTimeout(resolve, POLLING_INTERVAL);
+		});
+	}
+
+}
+
+module.exports = Transcoder;
---
+++ theory-test-transcode-candidate-video/test/test-lambda.js
@@ -0,0 +1,88 @@
+const assert = require('assert');
+const Transcoder = require('../src/transcoder');
+
+/**
+ * Stub implementation of AWS Service Object.
+ */
+class StubAWSElasticTranscoder {
+
+	/**
+	 * @param {boolean} operational
+	 */
+	constructor(operational = true, attempts = 1) {
+		this.operational = operational;
+		this.attempts = attempts;
+	}
+
+	createJob(params) {
+		assert.equal(params.Input.Key, 'input');
+		assert.equal(params.Output.Key, 'output');
+		if (this.operational) {
+			return {
+				promise() {
+					return new Promise((resolve) => {
+						resolve({ Job: { Id: 'id' } });
+					});
+				}
+			};
+		} else { // eslint-disable-line no-else-return
+			return {
+				promise() {
+					return Promise.reject();
+				}
+			};
+		}
+	}
+
+	readJob(params) {
+		this.attempts -= 1;
+		const status = (this.attempts === 0) ? 'Complete' : 'Progressing';
+		assert.equal(params.Id, 'id');
+		return this.operational ? {
+			promise() {
+				return new Promise((resolve) => {
+					resolve({ Job: { Status: status } });
+				});
+			}
+		} : {
+			promise() {
+				return Promise.reject();
+			}
+		};
+	}
+
+}
+
+
+describe('Transcoder', () => {
+	it('passes correctly structured parameters to AWS', (done) => {
+		const transcoder = new Transcoder(new StubAWSElasticTranscoder(true, 1));
+		transcoder.transcode('input', 'output')
+			.then(() => {
+				done();
+			})
+			.catch((error) => {
+				done(error); // unexpected
+			});
+	});
+	it('polls AWS if job is not complete', (done) => {
+		const transcoder = new Transcoder(new StubAWSElasticTranscoder(true, 3));
+		transcoder.transcode('input', 'output')
+			.then(() => {
+				done();
+			})
+			.catch((error) => {
+				done(error); // unexpected
+			});
+	});
+	it('interprets failure from AWS correctly', (done) => {
+		const transcoder = new Transcoder(new StubAWSElasticTranscoder(false, 1));
+		transcoder.transcode('input', 'output')
+			.then(() => {
+				done(new Error('Should not have succeeded!'));
+			})
+			.catch((error) => { // eslint-disable-line no-unused-vars
+				done(); // expected error
+			});
+	});
+});
